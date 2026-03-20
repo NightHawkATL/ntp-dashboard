090761e89970e55af3b8c9bdc75a8b055e63c80d
NightHawkATL <8395658+NightHawkATL@users.noreply.github.com>
2026-03-20 10:37:56 -0400

    Bump app version to v0.0.5
    Signed-off-by: NightHawkATL <8395658+NightHawkATL@users.noreply.github.com>

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from cryptography.fernet import Fernet
 
 app = Flask(__name__)
 
-APP_VERSION = "v0.0.4"          # <--- NEW: Hard-code your version here
+APP_VERSION = "v0.0.5"          # <--- NEW: Hard-code your version here
 
 # --- NEW: Directory and File Paths ---
 DATA_DIR = '/app/data'
